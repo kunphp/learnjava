cf791f00df9f2b8b8968d793a03fb91c48ec21f2
kunphp <1666845868@qq.com>
2020-07-06 22:28:30 +0800

    day02学习通过Spring的xml实现IOC

---
+++ day02_spring_account_xmlioc/src/main/java/com/kunjava/dao/impl/IAccountDaoImpl.java
@@ -0,0 +1,60 @@
+package com.kunjava.dao.impl;
+
+import com.kunjava.dao.IAccountDao;
+import com.kunjava.domain.Account;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import java.util.List;
+
+/**
+ * 账户持久层实现类
+ */
+public class IAccountDaoImpl implements IAccountDao {
+
+    private QueryRunner runner;
+
+    public void setRunner(QueryRunner runner) {
+        this.runner = runner;
+    }
+
+    public List<Account> findAllAccount() {
+        try {
+            return runner.query("select * from account",new BeanListHandler<Account>(Account.class));
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Account findAccountById(Integer id) {
+        try {
+            return runner.query("select * from account where id =?",new BeanHandler<Account>(Account.class),id);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveAccount(Account account) {
+        try {
+            runner.update("insert into account (name,money) values(?,?)",account.getName(),account.getMoney());
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateAccount(Account account) {
+        try {
+            runner.update("update account set name=?,money=? where id=?",account.getName(),account.getMoney(),account.getId());
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAccount(Integer id) {
+        try {
+            runner.update("delete from account where id=?",id);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ day02_spring_account_xmlioc/src/main/java/com/kunjava/domain/Account.java
@@ -0,0 +1,46 @@
+package com.kunjava.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * 账户的实体类
+ */
+public class Account implements Serializable {
+    private Integer id;
+    private String name;
+    private BigDecimal money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
