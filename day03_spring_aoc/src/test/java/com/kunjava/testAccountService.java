f99839b3447799476a000ed80b9fb5ed44ad3a0e
kunphp <1666845868@qq.com>
2020-07-07 23:57:37 +0800

    spring_xml_aop

--- day03_spring_aoc/src/test/java/com/kunjava/testAccountService.java
+++ day03_spring_aoc/src/test/java/com/kunjava/testAccountService.java
@@ -9,5 +9,10 @@ public class testAccountService {
         ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         IAccountService as = (IAccountService) ac.getBean("accountService");
         as.saveAccount();
+//        Logger类中的aroundPringLog方法开始记录日志了。。。前置
+//        执行了saveAccount
+//        Logger类中的aroundPringLog方法开始记录日志了。。。后置
+//        Logger类中的aroundPringLog方法开始记录日志了。。。最终
+//        Logger类中的aroundPringLog方法开始记录日志了。。。前置
     }
 }
