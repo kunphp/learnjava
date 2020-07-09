50b4373869ca0800e073f1c4e708464637f53921
王钰坤 <yukun.wang@etong-online.com>
2020-07-09 10:07:40 +0800

    spring基于注解声明事务控制

---
+++ day04_spring_tx_an/src/main/java/config/JdbcConfig.java
@@ -0,0 +1,35 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+@PropertySource("jdbc.properties")
+public class JdbcConfig {
+
+    @Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.user}")
+    private String user;
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean(name="jdbcTemplate")
+    public JdbcTemplate createJdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+    @Bean(name="dataSource")
+    public DataSource createDataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(driver);
+        ds.setUrl(url);
+        ds.setUsername(user);
+        ds.setPassword(password);
+        return ds;
+    }
+}
---
+++ day04_spring_tx_an/src/main/java/config/SpringConfiguration.java
@@ -0,0 +1,16 @@
+package config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan("com.kunjava")
+@Import({JdbcConfig.class,TransactionConfig.class})
+@PropertySource("jdbc.properties")
+@EnableTransactionManagement
+public class SpringConfiguration {
+}
